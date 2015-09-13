2967c41b6aca9dc0a180efa6b2410ce9d043734b
Jeevanandam M <jeeva@myjeeva.com>
2015-09-13 15:32:01 -0700

    started documenting package

--- middleware.go
+++ middleware.go
@@ -1,9 +1,7 @@
-/*
-Copyright (c) 2015 Jeevanandam M (jeeva@myjeeva.com), All rights reserved.
+// Copyright (c) 2015 Jeevanandam M (jeeva@myjeeva.com), All rights reserved.
+// resty source code and usage is governed by a MIT style
+// license that can be found in the LICENSE file.
 
-resty source code and usage is governed by a MIT style
-license that can be found in the LICENSE file.
-*/
 package resty
 
 import (
