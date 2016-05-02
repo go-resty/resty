216b1588b5e6dc3d0f5765d3c0a52cb2f1e1e104
Jeevanandam M <jeeva@myjeeva.com>
2016-05-01 18:49:40 -0700

    Revert "test format file"
    This reverts commit 0ae0b73e96ef9f6d96a46b4c308d6bc1d300822d.

--- resty_test.go
+++ resty_test.go
@@ -1,8 +1,8 @@
-package resty
-
 // Copyright (c) 2015-2016 Jeevanandam M (jeeva@myjeeva.com), All rights reserved.
 // resty source code and usage is governed by a MIT style
 // license that can be found in the LICENSE file.
+
+package resty
 
 import (
 	"bytes"
