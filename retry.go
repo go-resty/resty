621a6432d43fd828fc8657a823d775b43228bde9
ninedraft <clamores.pro@gmail.com>
2021-01-22 21:46:43 +0300

    refactor(retry.sleepDuration): :recycle: use local random generator

--- retry.go
+++ retry.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"math"
 	"math/rand"
+	"sync"
 	"time"
 )
 
@@ -170,12 +171,30 @@ func jitterBackoff(min, max time.Duration, attempt int) time.Duration {
 	capLevel := float64(max)
 
 	temp := math.Min(capLevel, base*math.Exp2(float64(attempt)))
-	ri := int64(temp / 2)
-	result := time.Duration(math.Abs(float64(ri + rand.Int63n(ri))))
+	ri := time.Duration(temp / 2)
+	result := randDuration(ri)
 
 	if result < min {
 		result = min
 	}
 
 	return result
+}
+
+var rnd = newRnd()
+var rndMu sync.Mutex
+
+func randDuration(center time.Duration) time.Duration {
+	rndMu.Lock()
+	defer rndMu.Unlock()
+
+	var ri = int64(center)
+	var jitter = rnd.Int63n(ri)
+	return time.Duration(math.Abs(float64(ri + jitter)))
+}
+
+func newRnd() *rand.Rand {
+	var seed = time.Now().UnixNano()
+	var src = rand.NewSource(seed)
+	return rand.New(src)
 }
