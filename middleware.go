ec85026d70a48caa97f765c5b3b02453e9d27b0c
Mohan Raj <praem1990@gmail.com>
2023-09-22 10:34:15 +0530

    Update middleware.go
    Refactor if block for better readability

--- middleware.go
+++ middleware.go
@@ -167,11 +167,10 @@ func parseRequestBody(c *Client, r *Request) (err error) {
 CL:
 	// by default resty won't set content length, you can if you want to :)
 	if c.setContentLength || r.setContentLength {
-		if r.bodyBuf != nil {
-			r.Header.Set(hdrContentLengthKey, fmt.Sprintf("%d", r.bodyBuf.Len()))
-		} else {
+		if r.bodyBuf == nil {
 			r.Header.Set(hdrContentLengthKey, "0")
-
+		} else {
+			r.Header.Set(hdrContentLengthKey, fmt.Sprintf("%d", r.bodyBuf.Len()))
 		}
 	}
 
