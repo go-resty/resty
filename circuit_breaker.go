1e259de7b764a50cdb6bdc3c4083e0f73d58b680
Jeevanandam M. <jeeva@myjeeva.com>
2025-12-27 02:04:40 -0800

    feat!: add ratio-based circuit breaker and streamline methods #1026 (#1086)

--- circuit_breaker.go
+++ circuit_breaker.go
@@ -12,6 +12,17 @@ import (
 	"time"
 )
 
+// ErrCircuitBreakerOpen is returned when the circuit breaker is open.
+var ErrCircuitBreakerOpen = errors.New("resty: circuit breaker open")
+
+type circuitBreakerState uint32
+
+const (
+	circuitBreakerStateClosed circuitBreakerState = iota
+	circuitBreakerStateOpen
+	circuitBreakerStateHalfOpen
+)
+
 // CircuitBreaker struct implements a state machine to monitor and manage the
 // states of circuit breakers. The three states are:
 //   - Closed: requests are allowed
@@ -23,72 +34,62 @@ import (
 //   - To Open State: when the failure count reaches the failure threshold.
 //   - Half-Open Check: when the specified timeout reaches, a single request is allowed
 //     to determine the transition state; if failed, it goes back to the open state.
+//
+// Use [NewCircuitBreakerWithCount] or [NewCircuitBreakerWithRatio] to create a new [CircuitBreaker]
+// instance accordingly.
 type CircuitBreaker struct {
-	policies         []CircuitBreakerPolicy
-	timeout          time.Duration
-	failureThreshold uint32
-	successThreshold uint32
-	state            atomic.Value // circuitBreakerState
-	failureCount     atomic.Uint32
-	successCount     atomic.Uint32
-	lastFailureAt    atomic.Value // time.Time
+	policies      []CircuitBreakerPolicy
+	resetTimeout  time.Duration
+	state         atomic.Value // circuitBreakerState
+	failureCount  atomic.Uint64
+	successCount  atomic.Uint64
+	lastFailureAt atomic.Value // time.Time
+
+	// Count based
+	failureThreshold uint64
+	successThreshold uint64
+
+	// Ratio based
+	isRatioBased bool
+	failureRatio float64 // Threshold, e.g., 0.5 for 50% failure
+	minRequests  uint64  // Minimum number of requests to consider failure ratio
+	totalCount   atomic.Uint64
 }
 
-// NewCircuitBreaker method creates a new [CircuitBreaker] with default settings.
+// NewCircuitBreakerWithCount method creates a new [CircuitBreaker] instance with Count settings.
 //
 // The default settings are:
-//   - Timeout: 10 seconds
-//   - FailThreshold: 3
-//   - SuccessThreshold: 1
 //   - Policies: CircuitBreaker5xxPolicy
-func NewCircuitBreaker() *CircuitBreaker {
+func NewCircuitBreakerWithCount(failureThreshold uint64, successThreshold uint64,
+	resetTimeout time.Duration, policies ...CircuitBreakerPolicy) *CircuitBreaker {
+	cb := newCircuitBreaker(resetTimeout, policies...)
+	cb.failureThreshold = failureThreshold
+	cb.successThreshold = successThreshold
+	return cb
+}
+
+// NewCircuitBreakerWithRatio method creates a new [CircuitBreaker] instance with Ratio settings.
+//
+// The default settings are:
+//   - Policies: CircuitBreaker5xxPolicy
+func NewCircuitBreakerWithRatio(failureRatio float64, minRequests uint64,
+	resetTimeout time.Duration, policies ...CircuitBreakerPolicy) *CircuitBreaker {
+	cb := newCircuitBreaker(resetTimeout, policies...)
+	cb.failureRatio = failureRatio
+	cb.minRequests = minRequests
+	cb.isRatioBased = true
+	return cb
+}
+
+func newCircuitBreaker(resetTimeout time.Duration, policies ...CircuitBreakerPolicy) *CircuitBreaker {
 	cb := &CircuitBreaker{
-		policies:         []CircuitBreakerPolicy{CircuitBreaker5xxPolicy},
-		timeout:          10 * time.Second,
-		failureThreshold: 3,
-		successThreshold: 1,
+		resetTimeout: resetTimeout,
+		policies:     []CircuitBreakerPolicy{CircuitBreaker5xxPolicy},
 	}
 	cb.state.Store(circuitBreakerStateClosed)
-	return cb
-}
-
-// SetPolicies method sets the one or more given CircuitBreakerPolicy(s) into
-// [CircuitBreaker], which will be used to determine whether a request is failed
-// or successful by evaluating the response instance.
-//
-//	// set one policy
-//	cb.SetPolicies(CircuitBreaker5xxPolicy)
-//
-//	// set multiple polices
-//	cb.SetPolicies(policy1, policy2, policy3)
-//
-//	// if you have slice, do
-//	cb.SetPolicies(policies...)
-//
-// NOTE: This method overwrites the policies with the given new ones. See [CircuitBreaker.AddPolicies]
-func (cb *CircuitBreaker) SetPolicies(policies ...CircuitBreakerPolicy) *CircuitBreaker {
-	cb.policies = policies
-	return cb
-}
-
-// SetTimeout method sets the timeout duration for the [CircuitBreaker]. When the
-// timeout reaches, a single request is allowed to determine the state.
-func (cb *CircuitBreaker) SetTimeout(timeout time.Duration) *CircuitBreaker {
-	cb.timeout = timeout
-	return cb
-}
-
-// SetFailureThreshold method sets the number of failures that must occur within the
-// timeout duration for the [CircuitBreaker] to transition to the Open state.
-func (cb *CircuitBreaker) SetFailureThreshold(threshold uint32) *CircuitBreaker {
-	cb.failureThreshold = threshold
-	return cb
-}
-
-// SetSuccessThreshold method sets the number of successes that must occur to transition
-// the [CircuitBreaker] from the Half-Open state to the Closed state.
-func (cb *CircuitBreaker) SetSuccessThreshold(threshold uint32) *CircuitBreaker {
-	cb.successThreshold = threshold
+	if len(policies) > 0 {
+		cb.policies = policies
+	}
 	return cb
 }
 
@@ -101,16 +102,6 @@ type CircuitBreakerPolicy func(resp *http.Response) bool
 func CircuitBreaker5xxPolicy(resp *http.Response) bool {
 	return resp.StatusCode > 499
 }
-
-var ErrCircuitBreakerOpen = errors.New("resty: circuit breaker open")
-
-type circuitBreakerState uint32
-
-const (
-	circuitBreakerStateClosed circuitBreakerState = iota
-	circuitBreakerStateOpen
-	circuitBreakerStateHalfOpen
-)
 
 func (cb *CircuitBreaker) getState() circuitBreakerState {
 	return cb.state.Load().(circuitBreakerState)
@@ -133,39 +124,58 @@ func (cb *CircuitBreaker) applyPolicies(resp *http.Response) {
 		}
 	}
 
+	if cb.isRatioBased {
+		cb.totalCount.Add(1)
+	}
+
 	if failed {
-		if cb.failureCount.Load() > 0 && time.Since(cb.lastFailureAt.Load().(time.Time)) > cb.timeout {
+		if cb.failureCount.Load() > 0 && time.Since(cb.lastFailureAt.Load().(time.Time)) > cb.resetTimeout {
 			cb.failureCount.Store(0)
 		}
 
 		switch cb.getState() {
 		case circuitBreakerStateClosed:
-			failCount := cb.failureCount.Add(1)
-			if failCount >= cb.failureThreshold {
-				cb.open()
+			failureCount := cb.failureCount.Add(1)
+			cb.lastFailureAt.Store(time.Now())
+
+			if cb.isRatioBased {
+				totalCount := cb.totalCount.Load()
+				if totalCount >= cb.minRequests {
+					currentFailureRatio := float64(failureCount) / float64(totalCount)
+					if currentFailureRatio >= cb.failureRatio {
+						cb.open()
+					}
+				}
 			} else {
-				cb.lastFailureAt.Store(time.Now())
+				if failureCount >= cb.failureThreshold {
+					cb.open()
+				}
 			}
 		case circuitBreakerStateHalfOpen:
 			cb.open()
 		}
-	} else {
-		switch cb.getState() {
-		case circuitBreakerStateClosed:
-			return
-		case circuitBreakerStateHalfOpen:
-			successCount := cb.successCount.Add(1)
-			if successCount >= cb.successThreshold {
-				cb.changeState(circuitBreakerStateClosed)
-			}
+
+		return
+	}
+
+	switch cb.getState() {
+	case circuitBreakerStateClosed:
+		return
+	case circuitBreakerStateHalfOpen:
+		successCount := cb.successCount.Add(1)
+		if successCount >= cb.successThreshold {
+			cb.changeState(circuitBreakerStateClosed)
 		}
 	}
 }
 
 func (cb *CircuitBreaker) open() {
 	cb.changeState(circuitBreakerStateOpen)
+	if cb.isRatioBased {
+		cb.totalCount.Store(0)
+	}
 	go func() {
-		time.Sleep(cb.timeout)
+		time.Sleep(cb.resetTimeout)
 		cb.changeState(circuitBreakerStateHalfOpen)
 	}()
 }
