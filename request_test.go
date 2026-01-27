f50eeef8007df55584c6ef6627f38b761d28da1c
Jeevanandam M. <jeeva@myjeeva.com>
2026-01-27 00:38:18 -0800

    Revert "feat(enhancement): add SetContentType helper method (#951)"
    This reverts commit 62fffe55f095546e11fb5ba1b7902265309cf423.

--- request_test.go
+++ request_test.go
@@ -1594,7 +1594,7 @@ func TestRequestFileUploadAsReader(t *testing.T) {
 
 	resp, err := dcnldr().
 		SetBody(file).
-		SetContentType("image/png").
+		SetHeader("Content-Type", "image/png").
 		Post(ts.URL + "/upload")
 
 	assertError(t, err)
@@ -1606,7 +1606,7 @@ func TestRequestFileUploadAsReader(t *testing.T) {
 
 	resp, err = dcnldr().
 		SetBody(file).
-		SetContentType("image/png").
+		SetHeader("Content-Type", "image/png").
 		SetContentLength(true).
 		Post(ts.URL + "/upload")
 
