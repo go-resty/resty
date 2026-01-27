432a43c8cde08dceafe24328d9cf88aebf99b405
Jeevanandam M. <jeeva@myjeeva.com>
2026-01-27 00:38:18 -0800

    Revert "feat(enhancement): add MaxConnsPerHost setting into TransportSettings #1058 (#1069)"
    This reverts commit 87a6f0fc7a176769e7ca71a71d89fc770a186c34.

--- client_test.go
+++ client_test.go
@@ -971,7 +971,6 @@ func TestCustomTransportSettings(t *testing.T) {
 		ExpectContinueTimeout:  1 * time.Second,
 		MaxIdleConns:           50,
 		MaxIdleConnsPerHost:    3,
-		MaxConnsPerHost:        100,
 		ResponseHeaderTimeout:  10 * time.Second,
 		MaxResponseHeaderBytes: 1 << 10,
 		WriteBufferSize:        2 << 10,
