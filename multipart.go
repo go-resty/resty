164315eb2b8f9b6e615ec4f0f668fe9bf546072e
Jeevanandam M. <jeeva@myjeeva.com>
2026-01-27 00:38:18 -0800

    Revert "feat(enhancement): multipart populate filename from path if file path value is empty (#944)"
    This reverts commit dc99b01a892a5ff5e94c55f4f0891a693b1c34aa.

--- multipart.go
+++ multipart.go
@@ -10,7 +10,6 @@ import (
 	"io"
 	"net/textproto"
 	"os"
-	"path/filepath"
 	"strings"
 )
 
@@ -103,10 +102,6 @@ func (mf *MultipartField) openFileIfRequired() error {
 	file, err := os.Open(mf.FilePath)
 	if err != nil {
 		return err
-	}
-
-	if isStringEmpty(mf.FileName) {
-		mf.FileName = filepath.Base(mf.FilePath)
 	}
 
 	// if file open is success, stat will succeed
