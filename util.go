5366fafc60c56904137daeeff62d2ee76f9f99e1
Jeevanandam M. <jeeva@myjeeva.com>
2026-01-27 00:38:18 -0800

    Revert "feat(hedging): support hedging in order to reduce latency when quering endpoints (#1081)"
    This reverts commit 12eb1c5e7c165c5423522b5cf1a0c087b5702da8.

--- util.go
+++ util.go
@@ -393,14 +393,8 @@ func (ire *invalidRequestError) Error() string {
 
 func drainBody(res *Response) {
 	if res != nil && res.Body != nil {
-		drainReadCloser(res.Body)
-	}
-}
-
-func drainReadCloser(body io.ReadCloser) {
-	if body != nil {
-		defer closeq(body)
-		_, _ = io.Copy(io.Discard, body)
+		defer closeq(res.Body)
+		_, _ = io.Copy(io.Discard, res.Body)
 	}
 }
 
