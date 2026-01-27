3950ecb581d0a31fb466056eed1e1c0133ad4daa
Jeevanandam M. <jeeva@myjeeva.com>
2026-01-27 00:38:18 -0800

    Revert "feat(enhancement): add missing SetHeaderAuthorizationKey from the mutex implementation and the flow of it (#947)"
    This reverts commit 251b9a0d99ce782c36d368f0c14ac9ca0448f4e6.

--- request.go
+++ request.go
@@ -62,7 +62,6 @@ type Request struct {
 	IsDone                     bool
 	IsSaveResponse             bool
 	Timeout                    time.Duration
-	HeaderAuthorizationKey     string
 	RetryCount                 int
 	RetryWaitTime              time.Duration
 	RetryMaxWaitTime           time.Duration
