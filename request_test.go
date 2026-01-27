dc33a7e94d54a2e92f117fac5af551dfec649d11
Jeevanandam M. <jeeva@myjeeva.com>
2026-01-27 00:38:18 -0800

    Revert "feat: add json definition and method to trace info (#964)"
    This reverts commit 03b017e601de42219f0b84a4e309b58214c1560b.

--- request_test.go
+++ request_test.go
@@ -1803,9 +1803,7 @@ func TestTraceInfo(t *testing.T) {
 			assertEqual(t, true, tr.TotalTime >= 0)
 			assertEqual(t, true, tr.TotalTime < time.Hour)
 			assertEqual(t, true, tr.TotalTime == resp.Time())
-			assertEqual(t, tr.RemoteAddr, serverAddr)
-
-			assertNotNil(t, tr.Clone())
+			assertEqual(t, tr.RemoteAddr.String(), serverAddr)
 		}
 
 		client.DisableTrace()
@@ -1825,7 +1823,7 @@ func TestTraceInfo(t *testing.T) {
 			assertEqual(t, true, tr.ResponseTime >= 0)
 			assertEqual(t, true, tr.TotalTime >= 0)
 			assertEqual(t, true, tr.TotalTime == resp.Time())
-			assertEqual(t, tr.RemoteAddr, serverAddr)
+			assertEqual(t, tr.RemoteAddr.String(), serverAddr)
 		}
 
 	})
@@ -1839,9 +1837,6 @@ func TestTraceInfo(t *testing.T) {
 			resp, err := c.R().EnableTrace().EnableDebug().Get(u)
 			assertNil(t, err)
 			assertNotNil(t, resp)
-
-			jsonStr := resp.Request.TraceInfo().JSON()
-			assertEqual(t, true, strings.Contains(jsonStr, serverAddr))
 		}
 
 		logContent := logBuf.String()
--- trace.go
+++ trace.go
@@ -9,6 +9,7 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"net"
 	"net/http/httptrace"
 	"time"
 )
@@ -18,45 +19,45 @@ import (
 type TraceInfo struct {
 	// DNSLookup is the duration that transport took to perform
 	// DNS lookup.
-	DNSLookup time.Duration `json:"dns_lookup_time"`
+	DNSLookup time.Duration
 
 	// ConnTime is the duration it took to obtain a successful connection.
-	ConnTime time.Duration `json:"connection_time"`
+	ConnTime time.Duration
 
 	// TCPConnTime is the duration it took to obtain the TCP connection.
-	TCPConnTime time.Duration `json:"tcp_connection_time"`
+	TCPConnTime time.Duration
 
 	// TLSHandshake is the duration of the TLS handshake.
-	TLSHandshake time.Duration `json:"tls_handshake_time"`
+	TLSHandshake time.Duration
 
 	// ServerTime is the server's duration for responding to the first byte.
-	ServerTime time.Duration `json:"server_time"`
+	ServerTime time.Duration
 
 	// ResponseTime is the duration since the first response byte from the server to
 	// request completion.
-	ResponseTime time.Duration `json:"response_time"`
+	ResponseTime time.Duration
 
 	// TotalTime is the duration of the total time request taken end-to-end.
-	TotalTime time.Duration `json:"total_time"`
+	TotalTime time.Duration
 
 	// IsConnReused is whether this connection has been previously
 	// used for another HTTP request.
-	IsConnReused bool `json:"is_connection_reused"`
+	IsConnReused bool
 
 	// IsConnWasIdle is whether this connection was obtained from an
 	// idle pool.
-	IsConnWasIdle bool `json:"is_connection_was_idle"`
+	IsConnWasIdle bool
 
 	// ConnIdleTime is the duration how long the connection that was previously
 	// idle, if IsConnWasIdle is true.
-	ConnIdleTime time.Duration `json:"connection_idle_time"`
+	ConnIdleTime time.Duration
 
 	// RequestAttempt is to represent the request attempt made during a Resty
 	// request execution flow, including retry count.
-	RequestAttempt int `json:"request_attempt"`
+	RequestAttempt int
 
 	// RemoteAddr returns the remote network address.
-	RemoteAddr string `json:"remote_address"`
+	RemoteAddr net.Addr
 }
 
 // String method returns string representation of request trace information.
@@ -77,21 +78,6 @@ func (ti TraceInfo) String() string {
 		ti.TLSHandshake, ti.ServerTime, ti.ResponseTime, ti.TotalTime,
 		ti.IsConnReused, ti.IsConnWasIdle, ti.ConnIdleTime, ti.RequestAttempt,
 		ti.RemoteAddr)
-}
-
-// JSON method returns the JSON string of request trace information
-func (ti TraceInfo) JSON() string {
-	buf := acquireBuffer()
-	defer releaseBuffer(buf)
-	_ = encodeJSON(buf, ti)
-	return buf.String()
-}
-
-// Clone method returns the clone copy of [TraceInfo]
-func (ti TraceInfo) Clone() *TraceInfo {
-	ti2 := new(TraceInfo)
-	*ti2 = ti
-	return ti2
 }
 
 // clientTrace struct maps the [httptrace.ClientTrace] hooks into Fields
