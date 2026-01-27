ee87787749b947ae3b4074f681346b5af8fea549
Jeevanandam M. <jeeva@myjeeva.com>
2026-01-27 00:38:18 -0800

    Revert "chore: fix typos in tests (#1063)"
    This reverts commit 6fe88cae8c4b3b753a7edfe33ceb57913d800b2c.

--- middleware_test.go
+++ middleware_test.go
@@ -194,7 +194,7 @@ func Test_parseRequestURL(t *testing.T) {
 			expectedURL: "https://example.com/foo/bar",
 		},
 		{
-			name: "using BaseURL with relative path in request URL with leading slash",
+			name: "using BaseURL with relative path in request URL wit leading slash",
 			initClient: func(c *Client) {
 				c.SetBaseURL("https://example.com")
 			},
@@ -505,7 +505,7 @@ func TestParseRequestBody(t *testing.T) {
 			expectedContentType: plainTextType,
 		},
 		{
-			name: "string body with GET method and AllowMethodGetPayload by request",
+			name: "string body with GET method and AllowMethodGetPayload by requst",
 			initRequest: func(r *Request) {
 				r.SetAllowMethodGetPayload(true)
 				r.SetBody("foo")
