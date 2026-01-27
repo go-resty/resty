df6be39399abe12f9423aaa8d196d91ab84be116
Jeevanandam M. <jeeva@myjeeva.com>
2026-01-27 00:38:18 -0800

    Revert "fix: handle nil credentials before clone (#990)"
    This reverts commit 3b61dd1e621ff01fe88ccd5ea84946c29fa71a28.

--- client.go
+++ client.go
@@ -2193,11 +2193,7 @@ func (c *Client) Clone(ctx context.Context) *Client {
 	cc.formData = cloneURLValues(c.formData)
 	cc.header = c.header.Clone()
 	cc.pathParams = maps.Clone(c.pathParams)
-
-	if c.credentials != nil {
-		cc.credentials = c.credentials.Clone()
-	}
-
+	cc.credentials = c.credentials.Clone()
 	cc.contentTypeEncoders = maps.Clone(c.contentTypeEncoders)
 	cc.contentTypeDecoders = maps.Clone(c.contentTypeDecoders)
 	cc.contentDecompressers = maps.Clone(c.contentDecompressers)
