a697d7513724ec3c2c25fa449ddd89f74d1e7781
Jeevanandam M. <jeeva@myjeeva.com>
2026-01-27 00:38:18 -0800

    Revert "feat(enhancement): use AddEventListener check instead of OnMessage check (#989)"
    This reverts commit 460219b914614699c0d64c27770a9e6a46f2485a.

--- sse.go
+++ sse.go
@@ -343,9 +343,8 @@ func (es *EventSource) Get() error {
 	if isStringEmpty(es.url) {
 		return fmt.Errorf("resty:sse: event source URL is required")
 	}
-
-	if len(es.onEvent) == 0 {
-		return fmt.Errorf("resty:sse: At least one OnMessage/AddEventListener func is required")
+	if _, found := es.onEvent[defaultEventName]; !found {
+		return fmt.Errorf("resty:sse: OnMessage function is required")
 	}
 
 	// reset to begin
